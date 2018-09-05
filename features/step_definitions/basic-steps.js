ab4a8f380f625c06d5da0739f1beaa9974081c2a
oliverochman <oliver.ochman@hotmail.com>
2018-09-05 09:43:26 +0200

    update feature and step defs

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -5,10 +5,29 @@ Given('I visit the site', async function() {
 })
 
 Then('I should see {string}', async function(content) {
-    return await this.sleep(3000);
     return await this.pageHasTextContent(content)
 })
 
 After(async function() {
     return await this.closeHomePage()
 })
+
+When('I click {string}', async function(string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+  })
+  
+  Then('I fill in {string} with {string}', async function(string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+  })
+  
+  Then('I should have {int} contact in my address book', async function(int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+  })
+  
+  Then('I should not see {string}', async function(string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'
+  })
